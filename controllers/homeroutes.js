c21709e27124a3385151725a3bb22f1dc21d04ec
Mona Lisa <stefanibusiness@gmail.com>
2022-08-03 18:59:31 -0700

    created models

--- controllers/homeroutes.js
+++ controllers/homeroutes.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 
 router.get('/signup', (req,res) => res.render('signup'));
 router.get('/', (req,res) => res.render('homepage'));
+router.get('/login', (req, res) => res.render('login'));
 
 module.exports = router;
