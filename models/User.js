216344dab3028fbf37cf80d3e262105be5ae697d
Mona Lisa <stefanibusiness@gmail.com>
2022-08-03 19:21:20 -0700

    created schema and dotenv, created an index.js for seeds

--- models/User.js
+++ models/User.js
@@ -2,17 +2,14 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
-
 class User extends Model {
     checkPassword(loginPw) {
         return bcrypt.compareSync(loginPw, this.password);
     }
 }
 
-n
 User.init(
-    {
-        
+    {   
         id: {
           type: DataTypes.INTEGER,
           allowNull: false,
