833e3d477a97ce8db6ae10c06b382d97253ae9c0
Mona Lisa <stefanibusiness@gmail.com>
2022-08-04 20:44:49 -0700

    made some route changes trying to make login work

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+
 const apiRoutes = require('./api/')
 const homeRoutes = require('./homeroutes');
 const dashRoutes = require('./dashboard-routes');
@@ -7,5 +8,10 @@ const dashRoutes = require('./dashboard-routes');
 router.use('/', homeRoutes);
 router.use('/dashboard', dashRoutes);
 router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    res.status(404).end();
+  });
+  
 
 module.exports = router;
